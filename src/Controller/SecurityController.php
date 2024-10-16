2809c7fbceddc53e4b50c8bce0f9f374a395372f
Vsevolod <ash.seva@gmail.com>
2024-10-17 00:59:34 +0400

    added second entity; added security context and one administrator; implemented register and login actions

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,26 +2,14 @@
 
 namespace App\Controller;
 
-use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Contracts\Service\Attribute\Required;
+
 
 class SecurityController extends AbstractController
 {
-    #[Required]
-    public UserRepository $userRepository;
-
-    #[Required]
-    public LoggerInterface $logger;
-
-    #[Required]
-    public EntityManagerInterface $entityManager;
-
     #[Route('/login', name: 'login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
